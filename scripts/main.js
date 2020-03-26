a451cca4d6554a8929673ed8825e16e7181b300b
Ncoder77 <niho24@outlook.de>
2020-03-26 20:57:39 +0100

    Added Theme variables and Navigation

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+//Navigation
+
+//HTML of Navigation links
+var navHTMl = document.querySelector("nav .wrap-container").innerHTML;
+
+if (navHTMl != undefined || navHTMl != null) {
+    //Number of menuLinks
+    var menuItems = navHTMl.split("</div>").length-1;
+    
+    //Get the middle of all Links (where new element has to be inserted)
+    menuMiddle = menuItems/2;
+
+    //Check if number is integer
+    if (menuMiddle % 1 == 0) {
+        var menuMiddle = document.querySelector("nav .wrap-container div:nth-child(" + menuMiddle + ")");
+        menuMiddle.style.margin = "0 100px 0 0";
+    } else {
+        console.error("LWD2.0 Error: The menu must have a even number of menu items");
+    }
+}
