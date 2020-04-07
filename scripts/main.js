1dc20df723fc06669feb12e0ea00374f7c1c7f28
Ncoder77 <niho24@outlook.de>
2020-04-07 12:04:10 +0200

    updated shadows and nav

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,16 @@ var navHTMl = document.querySelector("nav .wrap-container").innerHTML;
 
 if (navHTMl != undefined || navHTMl != null) {
     //Number of menuLinks
-    var menuItems = navHTMl.split("</div>").length-1;
+    var menuItems = navHTMl.split("</a>").length-1;
     
     //Get the middle of all Links (where new element has to be inserted)
     menuMiddle = menuItems/2;
 
     //Check if number is integer
     if (menuMiddle % 1 == 0) {
-        var menuMiddle = document.querySelector("nav .wrap-container div:nth-child(" + menuMiddle + ")");
+        var menuMiddle = document.querySelector("nav .wrap-container a:nth-child(" + menuMiddle + ")");
         menuMiddle.style.margin = "0 100px 0 0";
     } else {
-        console.error("LWD2.0 Error: The menu must have a even number of menu items");
+        console.error("LWD2.0 Error: The menu must have an even number of menu items");
     }
 }
