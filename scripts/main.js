5b8fff753e52b6feeb5dbb1f5eacea93d21144b0
JAcoder7 <50519072+JAcoder7@users.noreply.github.com>
2021-04-17 13:52:22 +0200

    change accentColors with javascript, nav started

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,65 @@
-//Navigation
+var lwd = {
+    cssVariables: document.querySelector(":root").style,
+    settingsPath: document.currentScript.src.substr(0, document.currentScript.src.lastIndexOf("/")) + "/../settings.json",
 
+    currentAccentColor: "blue",
+    currentTheme: "light",
+    
+
+    setAccentColor: function(color) {
+        fetch(this.settingsPath)
+        .then(res => res.json())
+        .then(out => {
+            if (typeof(out.colors[color]) != "undefined") {
+                this.currentAccentColor = color;
+                let keys = Object.keys(out.colors[color]);
+                for (let i = 0; i < keys.length; i++) {
+                    this.cssVariables.setProperty("--color-" + keys[i], out.colors[color][keys[i]]);
+                }
+            } else {
+                console.error("LWD:  Accent-color '" + color + "' was not found.");
+            }
+        })
+        .catch(err => { throw err });
+    },
+    
+    setTheme: function(theme) {
+        this.currentTheme = theme;
+    },
+}
+
+class LwdNav extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      // browser calls this method when the element is added to the document
+      // (can be called many times if an element is repeatedly added/removed)
+    }
+  
+    disconnectedCallback() {
+      // browser calls this method when the element is removed from the document
+      // (can be called many times if an element is repeatedly added/removed)
+    }
+  
+    static get observedAttributes() {
+      return [/* array of attribute names to monitor for changes */];
+    }
+  
+    attributeChangedCallback(name, oldValue, newValue) {
+      // called when one of attributes listed above is modified
+    }
+  
+    adoptedCallback() {
+      // called when the element is moved to a new document
+      // (happens in document.adoptNode, very rarely used)
+    }
+}
+customElements.define("lwd-nav", LwdNav);
+
+//Navigation
+/*
 //HTML of Navigation links
 var navHTMl = document.querySelector(".top-icon-nav .wrap-container").innerHTML;
 
@@ -19,4 +79,4 @@ if (navHTMl != undefined || navHTMl != null) {
     }
 } else {
     console.log("LWD2.0 Error: Navigation Error");
-}
+}*/
