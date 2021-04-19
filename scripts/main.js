83261b56c2c2d524a3a2c9524abc06c7e4def05d
JAcoder7 <50519072+JAcoder7@users.noreply.github.com>
2021-04-19 12:49:03 +0200

    nav started

--- scripts/main.js
+++ scripts/main.js
@@ -30,34 +30,120 @@ var lwd = {
 
 class LwdNav extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
-  
-    connectedCallback() {
-      // browser calls this method when the element is added to the document
-      // (can be called many times if an element is repeatedly added/removed)
-    }
-  
-    disconnectedCallback() {
-      // browser calls this method when the element is removed from the document
-      // (can be called many times if an element is repeatedly added/removed)
-    }
-  
-    static get observedAttributes() {
-      return [/* array of attribute names to monitor for changes */];
-    }
-  
-    attributeChangedCallback(name, oldValue, newValue) {
-      // called when one of attributes listed above is modified
-    }
-  
-    adoptedCallback() {
-      // called when the element is moved to a new document
-      // (happens in document.adoptNode, very rarely used)
-    }
-}
-customElements.define("lwd-nav", LwdNav);
 
+    expanded = false;
+    set (val) { this.expanded = val; this.onExpandedChange(); }
+    toggleExpanded() {
+        this.expanded = !this.expanded;
+    }
+
+    navTypes = {
+        side: {expandable: true}
+    }
+
+    connectedCallback() {
+        this.createMediaQuery();
+        this.createTypeSpecificElements();
+        this.onExpandedChange();
+    }
+
+    createTypeSpecificElements() {
+        this.querySelectorAll(".generated-nav-element").forEach(element => {  // first remove all elements
+            element.remove()
+        });
+
+        switch (this.getNavType()) {
+            case this.navTypes:
+                break;
+            
+            case this.navTypes.side:
+            default:
+                let menuHead = document.createElement("lwd-navitem");
+                menuHead.classList.add("hide-in-mobile", "generated-nav-element");
+
+                let navBtn = document.createElement("img");
+                navBtn.classList.add("navItemIcon");
+                navBtn.setAttribute("src", "img/menu-btn.svg");
+                navBtn.setAttribute("width", "18");
+                navBtn.addEventListener("click", () => {
+                    this.toggleExpanded();
+                });
+
+                let navLabel = document.createElement("h4");
+                navLabel.classList.add("navItemLabel");
+                navLabel.innerText = this.getNavTitle();
+                menuHead.appendChild(navLabel);
+                menuHead.appendChild(navBtn);
+                this.insertBefore(menuHead, this.firstChild);
+                break;
+        }
+    }
+
+    createMediaQuery() {
+        if (this.getNavType() != null) {
+            switch (this.getNavType()) {
+                case this.navTypes.side:                
+                default:
+                    if (this.querySelector("#navMediaQueryStyle") != null) {
+                        this.querySelector("#navMediaQueryStyle").innerHTML = "";
+                    } else {
+                        let newStyle = document.createElement("style");
+                        newStyle.innerHTML = "esdfghj";
+                        this.appendChild(newStyle);
+                    }
+                break;
+            }
+        }
+    }
+
+    getNavTitle() {
+        return "Lighted Web Design 2.0"; // TODO: getNavTitle
+    }
+
+    getNavType() {
+        if (this.getAttribute("type") != null) {
+            if (this.navTypes[this.getAttribute("type")] != undefined) {
+                return this.navTypes[this.getAttribute("type")];
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    onExpandedChange() {
+        if (this.getNavType() != null && this.getNavType().expandable) {
+            switch (this.getNavType()) {
+                case this.navTypes.side:
+                default:
+                    if (this.expanded) {
+                        this.style.setProperty("left", "0");
+                    } else {
+                        this.style.setProperty("left", "-380px");
+                    }
+                    break;
+            }
+        } else {
+            console.warn("LWD: current nav type is not expandable");
+        }
+    }
+
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name == "type") { // type has changed
+            this.createTypeSpecificElements();
+        }
+        if (name == "expandat") {
+            this.createMediaQuery();
+        }
+    }
+    static get observedAttributes() { return ['type', 'expandat']; }
+}
+
+// Define the new element
+customElements.define('lwd-nav', LwdNav);
 //Navigation
 /*
 //HTML of Navigation links
