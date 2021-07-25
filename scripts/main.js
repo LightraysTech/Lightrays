ccc31d3fa60d999b1d4b7160b22b38002867ee4e
Ncoder77 <niho24@outlook.de>
2021-07-25 20:21:30 +0200

    improved lighting colors

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ class Lwd {
                 this.setAccentColor(accentColor.split("[str]")[1]);
             }
         }
-        this.setShadowAngle(this.currentShadowAngle);
+        this.setLightTime(10.5);
     }
 
     static setAccentColor(color) {
@@ -97,40 +97,45 @@ class Lwd {
             if (sunset == undefined || sunset == null) {sunset = 18;}
             if (sunrise == undefined || sunrise == null) {sunrise = 6;}
 
+            //THEME
+            if (time < sunrise || time > sunset+2) {
+                this.setTheme("dark");
+            } else {
+                this.setTheme("light");
+
+            }
+
             //SHADOWS---
             let shadow_length = Math.sin(LwdFunctions.map_range(time, 0, 24, 0, Math.PI));
             shadow_length = 1+(shadow_length*2);
                 
-            let lightX = -Math.sin(angle * Math.PI/180)*100;
-            let lightY = Math.cos(angle * Math.PI/180)*100;
+            let lightX = -Math.sin(angle * Math.PI/180)*15;
+            let lightY = Math.cos(angle * Math.PI/180)*15;
 
             //SKY-------
             let sky_brightness = 100;
-            let sun_brightness = 95;
             if (time >= sunrise-2 && time <= sunrise) {  //while sunrise
-                console.log("sunrise");
                 sky_brightness = LwdFunctions.map_range(time, sunrise-2, sunrise, 10, 100);
             } else if(time >= sunset+2 && time <= sunset+4) { //while sunset
-                console.log("sunset");
                 sky_brightness = LwdFunctions.map_range(time, sunset+2, sunset+4, 100, 10);
             } else if (time < sunrise-2 || time > sunset+4) {  //night time
-                console.log("night");
                 sky_brightness = 10;
             }
 
             //SUN COLOR----
             let sun_tint = 215
             let sun_saturation = 100;
+            let sun_brightness = 95;
 
             if(time >= sunrise && time <= sunrise+2) {  //while sunrise
                 sun_saturation = LwdFunctions.map_range(time, sunrise, sunrise+2, 70, 100);
                 sun_tint = LwdFunctions.map_range(time, sunrise, sunrise+2, 11, 40);
-                sun_brightness = LwdFunctions.map_range(time, sunrise, sunrise+2, 80, 97);
+                sun_brightness = LwdFunctions.map_range(time, sunrise, sunrise+2, 80, 90);
 
             } else if(time >= sunset && time <= sunset+2) { //while sunset
                 sun_saturation = LwdFunctions.map_range(time, sunset, sunset+2, 100, 70);
                 sun_tint = LwdFunctions.map_range(time, sunset, sunset+2, 40, 11);
-                sun_brightness = LwdFunctions.map_range(time, sunset, sunset+2, 97, 80);
+                sun_brightness = LwdFunctions.map_range(time, sunset, sunset+2, 90, 80);
 
             } else if(time < sunrise || time > sunset+2) {  //night time
                 sun_saturation = 70;
