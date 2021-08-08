49d291381c28f839b6b15e6021271904d86c2386
Ncoder77 <niho24@outlook.de>
2021-08-08 13:03:59 +0200

    added Lwd window

--- scripts/main.js
+++ scripts/main.js
@@ -90,19 +90,23 @@ class Lwd {
         this.currentShadowAngle = angle;
     }
 
+    static activateDayNightCycle() {
+        let time = 0;
+        window.setInterval(() => {Lwd.setLightTime(time); time=time>=24?0:time+0.1;}, 50);
+    }
+
     static setLightTime(time, sunrise, sunset) {
         if (time >= 0 && time <=24) {
             let angle = LwdFunctions.map_range(time, 0, 24, -180, 180);
 
-            if (sunset == undefined || sunset == null) {sunset = 18;}
+            if (sunset == undefined || sunset == null) {sunset = 21;}
             if (sunrise == undefined || sunrise == null) {sunrise = 6;}
 
             //THEME
-            if (time < sunrise || time > sunset+2) {
+            if (time < sunrise || time > sunset) {
                 this.setTheme("dark");
             } else {
                 this.setTheme("light");
-
             }
 
             //SHADOWS---
@@ -112,34 +116,48 @@ class Lwd {
             let lightX = -Math.sin(angle * Math.PI/180)*15;
             let lightY = Math.cos(angle * Math.PI/180)*15;
 
-            //SKY-------
-            let sky_brightness = 100;
-            if (time >= sunrise-2 && time <= sunrise) {  //while sunrise
-                sky_brightness = LwdFunctions.map_range(time, sunrise-2, sunrise, 10, 100);
-            } else if(time >= sunset+2 && time <= sunset+4) { //while sunset
-                sky_brightness = LwdFunctions.map_range(time, sunset+2, sunset+4, 100, 10);
-            } else if (time < sunrise-2 || time > sunset+4) {  //night time
-                sky_brightness = 10;
+
+            let sun_tint = 215;
+            let sun_saturation = 0;
+            let sun_brightness = 100;
+            let sky_brightness = 92;
+            let sky_saturation = 87;
+
+            //SUN------
+            if(time >= sunrise && time <= sunrise+2) {  //while sunrise
+                sun_saturation = LwdFunctions.map_range(time, sunrise, sunrise+2, 92, 100);
+                sun_tint       = LwdFunctions.map_range(time, sunrise, sunrise+2, 11, 40);
+                sun_brightness = LwdFunctions.map_range(time, sunrise, sunrise+2, 80, 100);
+                sky_brightness = 100;
+            } else if(time >= sunset-2 && time <= sunset) { //while sunset
+                sun_saturation = LwdFunctions.map_range(time, sunset-2, sunset, 100, 92);
+                sun_tint       = LwdFunctions.map_range(time, sunset-2, sunset, 40, 11);
+                sun_brightness = LwdFunctions.map_range(time, sunset-2, sunset, 100, 80);
+                sky_brightness = 100;
+            } else if(time < sunrise || time > sunset) {  //night time
+                sun_saturation = 70;
             }
 
-            //SUN COLOR----
-            let sun_tint = 215
-            let sun_saturation = 100;
-            let sun_brightness = 95;
-
-            if(time >= sunrise && time <= sunrise+2) {  //while sunrise
-                sun_saturation = LwdFunctions.map_range(time, sunrise, sunrise+2, 70, 100);
-                sun_tint = LwdFunctions.map_range(time, sunrise, sunrise+2, 11, 40);
-                sun_brightness = LwdFunctions.map_range(time, sunrise, sunrise+2, 80, 90);
-
-            } else if(time >= sunset && time <= sunset+2) { //while sunset
-                sun_saturation = LwdFunctions.map_range(time, sunset, sunset+2, 100, 70);
-                sun_tint = LwdFunctions.map_range(time, sunset, sunset+2, 40, 11);
-                sun_brightness = LwdFunctions.map_range(time, sunset, sunset+2, 90, 80);
-
-            } else if(time < sunrise || time > sunset+2) {  //night time
-                sun_saturation = 70;
+            //Sun brightness night time
+            if (time > sunrise-1 && time < sunrise) {
+                sun_brightness = LwdFunctions.map_range(time, sunrise-1, sunrise, 20, 40);
+            } else if (time > sunset && time < sunset+1) {
+                sun_brightness = LwdFunctions.map_range(time, sunset, sunset+1, 40, 20);
+            } else if (time <= sunrise-1 || time >= sunset+1) {
                 sun_brightness = 20;
+            }
+
+
+            //SKY-------
+            if (time >= sunrise && time <= sunrise+1) {  //while sunrise
+                sky_brightness = LwdFunctions.map_range(time, sunrise, sunrise+1, 80, 100);
+                sky_saturation = LwdFunctions.map_range(time, sunrise, sunrise+1, 30, 87);
+            } else if(time >= sunset-1 && time <= sunset) { //while sunset
+                sky_brightness = LwdFunctions.map_range(time, sunset-1, sunset, 100, 80);
+                sky_saturation = LwdFunctions.map_range(time, sunset-1, sunset, 87, 30);
+            } else if (time < sunrise || time > sunset) {  //night time
+                sky_brightness = 20;
+                sky_saturation = 0;
             }
 
             //Apply values to css
@@ -148,6 +166,7 @@ class Lwd {
             this.cssVariables.setProperty("--shadow-angle", angle + "deg");
             this.cssVariables.setProperty("--shadow-length", shadow_length);
             this.cssVariables.setProperty("--sky-brightness", sky_brightness + "%");
+            this.cssVariables.setProperty("--sky-saturation", sky_saturation + "%");
             this.cssVariables.setProperty("--sun-tint", sun_tint);
             this.cssVariables.setProperty("--sun-saturation", sun_saturation + "%");
             this.cssVariables.setProperty("--sun-brightness", sun_brightness + "%");
@@ -198,10 +217,84 @@ class LwdFunctions {
     static map_range(value, low1, high1, low2, high2) {
         return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
     }
+
+    static dragElement(elem, draggableElem) {
+        var elemWidth = 0, elemHeight = 0, pointerOffsetX = 0, pointerOffsetY = 0, x, y;
+
+        if (draggableElem != undefined) {
+            draggableElem.onmousedown = dragMouseDown;
+        } else {
+            elem.onmousedown = dragMouseDown;
+        }
+      
+        function dragMouseDown(e) {
+          e = e || window.event;
+          e.preventDefault();
+
+        elemWidth = elem.offsetWidth;
+        elemHeight = elem.offsetHeight;
+        pointerOffsetX = e.offsetX;
+        pointerOffsetY = e.offsetY;
+
+        // Get the bounding rectangle of target
+        let rect = e.target.getBoundingClientRect();
+
+        // Mouse position
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+
+        console.log(pointerOffsetX, pointerOffsetY, x, y);
+
+          document.onmouseup = closeDragElement;
+          document.onmousemove = elementDrag;
+        }
+      
+        function elementDrag(e) {
+          e = e || window.event;
+          e.preventDefault();
+          console.log("dragging");
+      
+          let positionPxLeft = e.clientX-pointerOffsetX+(elemWidth/2);
+          let positionPxTop = e.clientY-pointerOffsetY+(elemHeight/2);
+          let windowWidth = window.innerWidth;
+          let windowHeight = window.innerHeight;
+          
+          let finalPosTop = LwdFunctions.map_range(positionPxTop, 0, windowHeight, 0, 100);
+          let finalPosLeft = LwdFunctions.map_range(positionPxLeft, 0, windowWidth, 0, 100);
+      
+          finalPosTop = Math.round(finalPosTop * 100) / 100;
+          finalPosLeft = Math.round(finalPosLeft * 100) / 100;
+      
+          function limitPos(elempos) {
+              if (elempos <= 0) {
+                  return 0;
+              } else if (elempos >= 100) {
+                  return 100;
+              } else {
+                  return elempos;
+              }
+          }
+          elem.style.setProperty("left", limitPos(finalPosLeft) + "%");
+          elem.style.setProperty("top", limitPos(finalPosTop) + "%");
+        }
+      
+        function closeDragElement() {
+          // stop moving when mouse button is released:
+          document.onmouseup = null;
+          document.onmousemove = null;
+        }
+      }
 }
 
 window.addEventListener("load", () => {
+    document.querySelectorAll("lwd-nav").forEach(e => e.initialize());
     Lwd.init();
+    let navIcons = document.querySelectorAll(".navItemIcon")
+    navIcons.forEach((i) => {
+        i.addEventListener("click", () => {
+        console.log("pressed");
+        });
+    });
 });
 
 class LwdNav extends HTMLElement {
@@ -286,11 +379,17 @@ class LwdNav extends HTMLElement {
     }
 
     initialize() {
-        window.addEventListener("load", () => {
+        if (document.readyState == "complete") {
             this.removeGeneratedElements();
             this.getNavType()?.initialize(this);
             this.getMobileNavType().initialize(this);  
-        });
+        } else {
+            window.addEventListener("load", () => {
+                this.removeGeneratedElements();
+                this.getNavType()?.initialize(this);
+                this.getMobileNavType().initialize(this);  
+            });
+        }
     }
 
     connectedCallback() {
@@ -323,6 +422,7 @@ class LwdNav extends HTMLElement {
             navButton = document.createElement("img");
             navButton.setAttribute("src", Lwd.rootFolderPath + "/img/menu-btn.svg");
             navButton.setAttribute("width", width);
+
         } else {
             console.info("LWD: navButton found");
             navButton = specifiedNavButton.cloneNode();
@@ -332,6 +432,7 @@ class LwdNav extends HTMLElement {
             specifiedNavButton.style.setProperty("display", "none");
         }
         navButton.classList.add("navItemIcon");
+        navButton.classList.add("generatedNavButton")
         navButton.addEventListener("click", () => this.toggleAltState());
         return navButton;
     }
@@ -393,10 +494,116 @@ class LwdNav extends HTMLElement {
                 }
                 break;
         }
-        this.initialize();
+        if (name != "alt-state") {
+            this.initialize();
+        }
     }
     static get observedAttributes() { return ['type', 'mobile-type', 'alt-state']; }
 }
 
 // Define the new element
 customElements.define('lwd-nav', LwdNav);
+
+
+// Erstelle eine Klasse für das Element
+class LwdWindow extends HTMLElement {
+    constructor() {
+      super();
+    }
+    generateElements() {
+        let oldHTML = this.innerHTML;
+        if (this.querySelector("svg") == undefined) {
+            this.innerHTML = oldHTML + "<svg viewBox='0 0 512 512' onclick='this.parentNode.closeWindow()'><path d='M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z'/></svg>";
+        }
+    }
+    static isOpen = true;
+    closeWindow() {
+        if (this.isOpen)  {
+            this.classList.add("animation", "window-close");
+            this.style.setProperty("visibility", "hidden");
+            setTimeout(() => {
+                this.style.setProperty("top", "150%");
+                this.classList.remove("animation", "window-close");
+            }, 400);
+            this.isOpen = false;
+        }
+    }
+    openWindow() {
+        document.querySelectorAll("lwd-window").forEach(w => w.style.setProperty("z-index", "101"));
+        this.style.setProperty("z-index", "102");
+
+        if (this.isOpen) {
+            this.classList.add("animation", "window-highlight");
+            setTimeout(() => {this.classList.remove("animation", "window-highlight")}, 300);
+        } else {
+            this.classList.add("animation", "window-open");
+            this.style.setProperty("visibility", "visible");
+            setTimeout(() => {
+                this.classList.remove("animation", "window-open");
+                this.style.removeProperty("top");
+            }, 200);
+            this.isOpen = true;
+        }
+    }
+    updateStyle() {
+        this.addEventListener("mousedown", () => {
+            document.querySelectorAll("lwd-window").forEach(w => w.style.setProperty("z-index", "101"));
+            this.style.setProperty("z-index", "102");
+        });
+        this.querySelector("h2").addEventListener("mousedown", () => {
+            this.style.setProperty("transform", "translate(-50%, -50%) scale(1.01)");
+            this.style.setProperty("--height", "1.5");
+        });
+        this.querySelector("h2").addEventListener("mouseup", () => {
+            this.style.setProperty("transform", "translate(-50%, -50%) scale(1)");
+            this.style.setProperty("--height", "1");
+        });
+    }
+    setAttributes() {
+        if (this.getAttribute("draggable") != undefined) {
+            LwdFunctions.dragElement(this, this.querySelector("h2"));
+        }
+        if (this.getAttribute("width") != undefined) {
+            this.style.setProperty("width", this.getAttribute("width"));
+        }
+        if (this.getAttribute("height") != undefined) {
+            this.style.setProperty("height", this.getAttribute("height"));
+        }        
+        if (this.getAttribute("closed") != undefined) {
+            if (this.getAttribute("closed") == "false") {
+                this.style.setProperty("visibility", "visible");
+                this.isOpen = true;
+            } else {
+                this.isOpen = false;
+                this.style.setProperty("visibility", "hidden");
+            }
+        } else {
+            this.isOpen = true;
+        }
+    }
+    initialize() {
+        if (document.readyState == "complete") {
+            console.log("elem initialized");
+            this.generateElements();
+            this.updateStyle();
+            this.setAttribute();
+        } else {
+            window.addEventListener("load", () => {
+                console.log("elem initialized onload");
+                this.generateElements();
+                this.updateStyle();
+                this.setAttributes();
+            });
+        }
+    }
+    connectedCallback() {
+        this.initialize();
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.initialize();
+    }
+    static get observedAttributes() { return ["draggable", "width", "height", "closed"]; }
+}
+  
+// Definiere das neue Element
+customElements.define('lwd-window', LwdWindow);
