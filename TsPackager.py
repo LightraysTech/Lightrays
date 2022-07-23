bd14e6d76c5aa7e70bd7372fcdbf91583be2d86b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-23 21:49:53 +0200

    Fixed TsPackager import Paths

--- TsPackager.py
+++ TsPackager.py
@@ -3,6 +3,10 @@ import os
 baseFile = 'scripts/LR.ts'
 outputPath = 'scripts/packaged.ts'
 
+baseFile = os.path.abspath(baseFile)
+
+print("\n-------------TsPackager-------------\n")
+
 class JsImportData:
     def __init__(self, filePath, member, moduleName):
         self.filePath = filePath
@@ -16,13 +20,15 @@ def getImports(file, recursive):
         lines = f.readlines()
         for line in lines:
             if(line.startswith("import")):
-                fileName = line.split("from \"")[1].split("\";")[0] + ".ts"
-                fileName = fileName.replace("./", os.path.dirname(file) + "/")
-                importFiles.append(fileName)
+                importFilePath = line.split("from \"")[1].split("\";")[0] + ".ts"
+                importFilePath = os.path.join(os.path.dirname(os.path.abspath(file)),importFilePath)
+                importFilePath = os.path.normpath(importFilePath)
+                if(not(importFilePath in importFiles) and not(importFilePath == baseFile)):
+                    importFiles.append(importFilePath)
+                    subImports = getImports(importFilePath, True)
+                    if(len(subImports) != 0):
+                        importFiles.extend(subImports)
 
-                subImports = getImports(fileName, True)
-                if(len(subImports) != 0):
-                    importFiles.extend(subImports)
     return importFiles
 
 imports = getImports(baseFile, True)
@@ -42,7 +48,7 @@ for file in imports:
 
 outputFileContent = outputFileContent.replace("export ", "")
 outputFileContent = outputFileContent.replace("default ", "")
-print(outputFileContent)
+#print(outputFileContent)
 
 f = open(outputPath, "w")
 f.write(outputFileContent)
