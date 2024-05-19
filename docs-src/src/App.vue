2b1f9a43af423a57fdd9d82ba255c42f07d05e30
JAcoder7 <jaho24@outlook.de>
2024-05-19 22:33:00 +0200

    began vue docs

---
+++ docs-src/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+
+  </header>
+  <main>
+    <RouterView />
+  </main>
+
+</template>
+
+<style scoped>
+</style>
---
+++ docs-src/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+home
+</template>
