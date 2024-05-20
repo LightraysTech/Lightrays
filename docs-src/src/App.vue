25fd5600fa599244bbc53ed67f050a465bb416b6
JAcoder7 <jaho24@outlook.de>
2024-05-20 03:47:46 +0200

    navigation

--- docs-src/src/App.vue
+++ docs-src/src/App.vue
@@ -1,18 +1,79 @@
 <script setup>
+import { ref, watch } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
+import router from './router';
+
+const theme = ref(window.matchMedia("(prefers-color-scheme: dark)").matches ? "auto-dark" : "auto-light");
+
+const navOpen = ref(true);
 </script>
 
 <template>
-  <header>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <nav :open="navOpen">
+    <a @click="navOpen = false">
+      <img src="/logo.png" height="20" style="display: block;filter: hue-rotate(calc(var(--hue) * 1deg - 230deg))">
+      <b>Lightrays</b>
+    </a>
+    <br>
+    <a>
+      <i class="fluentIcon home_filled"></i>
+      <span>Home</span>
+    </a>
+    <a>
+      <i class="fluentIcon layout_column_two_split_right_filled"></i>
+      Layout
+    </a>
+    <a>
+      <i class="fluentIcon text_field_filled"></i>
+      Input
+    </a>
+    <details>
+      <summary>Submenu</summary>
+      <a>Page</a>
+      <details>
+        <summary>Menu</summary>
+        <a>Link 1</a>
+        <a>Link 2</a>
+      </details>
+    </details>
+    <div style="flex-grow: 1;"></div>
+    <a href="">
+      <i class="fluentIcon settings_filled"></i>
+      Settings
+    </a>
+  </nav>
 
+  <header>
+    <img class="mobile-header" @click="navOpen = true" src="/logo.png" height="24" style="filter: hue-rotate(calc(var(--hue) * 1deg - 230deg))">
+    <b class="mobile-header">{{ router.currentRoute.value.name }}</b>
+
+    <div style="flex-grow: 1;"></div>
+    <button v-if="theme == 'light' || theme == 'auto-light'" @click="theme = 'dark'"><i class="fluentIcon weather_moon_regular"></i></button>
+    <button v-if="theme == 'dark' || theme == 'auto-dark'" @click="theme = 'light'"><i class="fluentIcon weather_sunny_regular"></i></button>
   </header>
-  <main>
+  <main :theme>
     <RouterView />
   </main>
 
 </template>
 
 <style scoped>
+header {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 8px 16px;
+  border-radius: 0;
+  border-bottom: var(--site-stroke);
+}
+
+@media (width > 1000px) {
+  .mobile-header {
+    display: none;
+  }
+
+  header {
+    border: none;
+  }
+}
 </style>
--- docs-src/src/views/HomeView.vue
+++ docs-src/src/views/HomeView.vue
@@ -2,5 +2,72 @@
 </script>
 
 <template>
-home
+  <h1>Lightrays</h1>
+  <br>
+  <p>
+    A modern CSS Web Design including themes, accent colors and animations.
+  </p>
+  <br><br>
+  <h3>Examples</h3>
+  <br><br>
+  <h4>Component types</h4>
+  <br>
+  <div class="flex-grow gap">
+    <div class="flex-vertical gap">
+      <div class="lr-box">Normal</div>
+      <div class="lr-box flat">Flat</div>
+      <div class="lr-box important">Important</div>
+      <div class="lr-box transparent">Transparent</div>
+      <div class="lr-box small">Small</div>
+      <div class="lr-box inline">Inline</div>
+      <div class="lr-box small inline">Small Inline</div>
+    </div>
+    <div class="flex-vertical gap">
+      <div class="interactive lr-box">Normal</div>
+      <div class="interactive lr-box flat">Flat</div>
+      <div class="interactive lr-box important">Important</div>
+      <div class="interactive lr-box transparent">Transparent</div>
+      <div class="interactive lr-box small">Small</div>
+      <div class="interactive lr-box inline">Inline</div>
+      <div class="interactive lr-box small inline">Small Inline</div>
+    </div>
+  </div>
+  <br><br>
+  <h4>Colors</h4>
+  <div>
+    <table>
+      <tr>
+        <td><label>Hue</label></td>
+        <td><input type="range" value="250" onpointermove="document.documentElement.style.setProperty('--hue', event.target.value)" min="0" max="355"></td>
+      </tr>
+      <tr>
+    <td><label>Hue-range</label></td>
+    <input type="range" value="6" onpointermove="document.documentElement.style.setProperty('--hue-range', event.target.value)" min="0" max="20">
+      </tr>
+    </table>
+    <br>
+    <div class="flex-grow" style="width: min(350px, 100%); border-radius: 8px; overflow: hidden; box-shadow: var(--shadow-3);">
+      <div style="height: 120px; background: var(--darkest);"> </div>
+      <div style="height: 120px; background: var(--dark);"> </div>
+      <div style="height: 120px; background: var(--primary);"> </div>
+      <div style="height: 120px; background: var(--light);"> </div>
+      <div style="height: 120px; background: var(--lightest);"> </div>
+    </div>
+    <br>
+    <div class="flex-grow" style="width: min(350px, 100%);border-radius: 8px; overflow: hidden; box-shadow: var(--shadow-3);">
+      <div style="height: 120px; background: var(--gradient);"> </div>
+    </div>
+
+  </div>
+  <br><br>
+  <h4>Shadows</h4>
+  <div class="lr-list gap">
+    <div class="flat">Flat</div>
+    <div style="box-shadow: none;">No Shadow</div>
+    <div style="box-shadow: var(--shadow-0);">Shadow 0</div>
+    <div style="box-shadow: var(--shadow-1);">Shadow 1</div>
+    <div style="box-shadow: var(--shadow-2);">Shadow 2</div>
+    <div style="box-shadow: var(--shadow-3);">Shadow 3</div>
+    <div style="box-shadow: var(--shadow-4);">Shadow 4</div>
+  </div>
 </template>
