442d9f7224c836608becc63a4bac391aea9f894d
JAcoder7 <jaho24@outlook.de>
2024-11-25 01:06:18 +0100

    fixed some component sizes

--- docs-src/src/views/ExperimentsView.vue
+++ docs-src/src/views/ExperimentsView.vue
@@ -2,19 +2,42 @@
 </script>
 
 <template>
-<h1>Experiments</h1>
-<br>
-<br>
-<h4>details</h4>
-<br>
-<details>
-  <summary>Summary</summary>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore tempore nulla tempora, dolorum adipisci aperiam vel itaque omnis sint inventore at veritatis libero, ipsa repudiandae iste ipsam voluptatem, molestias quam!
-  Rerum harum expedita natus laudantium quos repudiandae sunt architecto consequatur est aspernatur inventore totam vero, libero sint corrupti voluptatibus et quasi ipsam voluptates perspiciatis quod magni, explicabo iste voluptatum! Ab!
-  Asperiores aut reprehenderit quidem reiciendis amet eum ipsam perferendis quas facilis ipsum sed incidunt dolorum explicabo illo perspiciatis, vel rerum commodi facere nisi, velit mollitia ullam. Impedit quas officiis aspernatur?
-  A nam corrupti culpa iste praesentium alias ipsam quae, neque possimus obcaecati, maxime ea fuga similique, vero nemo! Explicabo nesciunt hic tempore aperiam sed maxime dolores quidem culpa officiis sint.
-</details>
+  <h1>Experiments</h1>
+  <br>
+  <br>
+  <h4>details</h4>
+  <br>
+  <details>
+    <summary>Summary</summary>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore tempore nulla tempora, dolorum adipisci aperiam vel itaque omnis sint inventore at veritatis libero, ipsa repudiandae iste ipsam voluptatem, molestias quam!
+    Rerum harum expedita natus laudantium quos repudiandae sunt architecto consequatur est aspernatur inventore totam vero, libero sint corrupti voluptatibus et quasi ipsam voluptates perspiciatis quod magni, explicabo iste voluptatum! Ab!
+    Asperiores aut reprehenderit quidem reiciendis amet eum ipsam perferendis quas facilis ipsum sed incidunt dolorum explicabo illo perspiciatis, vel rerum commodi facere nisi, velit mollitia ullam. Impedit quas officiis aspernatur?
+    A nam corrupti culpa iste praesentium alias ipsam quae, neque possimus obcaecati, maxime ea fuga similique, vero nemo! Explicabo nesciunt hic tempore aperiam sed maxime dolores quidem culpa officiis sint.
+  </details>
+  <div>
+    <input type="checkbox">
+    <input type="text">
+    <button><i class="fluentIcon save_regular"></i></button>
+    <button>Button</button>
+    <select>
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </select>
+  </div>
+  <br><br>
+  <div class="small-components">
+    <input type="checkbox">
+    <input type="text">
+    <button><i class="fluentIcon save_regular"></i></button>
+    <button>Button</button>
+    <select>
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </select>
+  </div>
+
 </template>
 
-<style scoped lang='scss'>
-</style>
+<style scoped lang='scss'></style>
