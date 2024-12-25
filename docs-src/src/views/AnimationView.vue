4cb9c057a9c647864bc8fbeb1bd367f16cc7f001
JAcoder7 <jaho24@outlook.de>
2024-12-25 16:14:56 +0100

    improved details animation

--- docs-src/src/views/AnimationView.vue
+++ docs-src/src/views/AnimationView.vue
@@ -7,8 +7,18 @@ onMounted(()=>{
   /** @type {CanvasRenderingContext2D} */
   const ctx1 = graph1.value.getContext("2d")
 
-let ease =window.getComputedStyle(document.body).getPropertyValue("--curve-ease-in")
-ctx1.setTransform(3,0,0,-3,0,300)
+let ease =window.getComputedStyle(document.body).getPropertyValue("--curve-ease")
+ctx1.setTransform(3,0,0,-3,0,350)
+
+ctx1.beginPath();
+ctx1.moveTo(0, 0);
+ctx1.bezierCurveTo(...ease.match(/(?<=\().*(?=\))/)[0].split(",").map(x=>Number(x)*100), 100, 100);
+ctx1.stroke();
+
+
+ease = "cubic-bezier(0.45, 0, 0.55, 1);"
+ctx1.setTransform(3,0,0,-3,0,350)
+ctx1.strokeStyle = "tomato"
 
 ctx1.beginPath();
 ctx1.moveTo(0, 0);
@@ -19,7 +29,7 @@ console.log(ctx1.getTransform());
 </script>
 
 <template>
-<canvas ref="graph1" width="300" height="300"></canvas>
+<canvas ref="graph1" width="300" height="350"></canvas>
 </template>
 
 <style scoped>
